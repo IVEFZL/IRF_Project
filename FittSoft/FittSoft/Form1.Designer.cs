ad19496aefbeef6fc18ec8e9b40606daf9d4b7c8
Tibor Sirkó <tibor.sirko@stud.uni-corvinus.hu>
2020-11-30 19:54:57 +0100

    Created base forms

--- FittSoft/FittSoft/Form1.Designer.cs
+++ FittSoft/FittSoft/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_to_FormWorkoutSession = new System.Windows.Forms.Button();
+            this.btn_to_FormMeasures = new System.Windows.Forms.Button();
+            this.btn_to_FormDoneWorkouts = new System.Windows.Forms.Button();
+            this.btn_to_FormExercises = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_to_FormWorkoutSession
+            // 
+            this.btn_to_FormWorkoutSession.Location = new System.Drawing.Point(182, 112);
+            this.btn_to_FormWorkoutSession.Name = "btn_to_FormWorkoutSession";
+            this.btn_to_FormWorkoutSession.Size = new System.Drawing.Size(137, 23);
+            this.btn_to_FormWorkoutSession.TabIndex = 0;
+            this.btn_to_FormWorkoutSession.Text = "Edzés elkezdése";
+            this.btn_to_FormWorkoutSession.UseVisualStyleBackColor = true;
+            this.btn_to_FormWorkoutSession.Click += new System.EventHandler(this.btn_to_FormWorkoutSession_Click);
+            // 
+            // btn_to_FormMeasures
+            // 
+            this.btn_to_FormMeasures.Location = new System.Drawing.Point(434, 112);
+            this.btn_to_FormMeasures.Name = "btn_to_FormMeasures";
+            this.btn_to_FormMeasures.Size = new System.Drawing.Size(145, 23);
+            this.btn_to_FormMeasures.TabIndex = 1;
+            this.btn_to_FormMeasures.Text = "Test mérések";
+            this.btn_to_FormMeasures.UseVisualStyleBackColor = true;
+            this.btn_to_FormMeasures.Click += new System.EventHandler(this.btn_to_FormMeasures_Click);
+            // 
+            // btn_to_FormDoneWorkouts
+            // 
+            this.btn_to_FormDoneWorkouts.Location = new System.Drawing.Point(182, 218);
+            this.btn_to_FormDoneWorkouts.Name = "btn_to_FormDoneWorkouts";
+            this.btn_to_FormDoneWorkouts.Size = new System.Drawing.Size(125, 47);
+            this.btn_to_FormDoneWorkouts.TabIndex = 2;
+            this.btn_to_FormDoneWorkouts.Text = "Korábbi edzések megtekintése";
+            this.btn_to_FormDoneWorkouts.UseVisualStyleBackColor = true;
+            this.btn_to_FormDoneWorkouts.Click += new System.EventHandler(this.btn_to_FormDoneWorkouts_Click);
+            // 
+            // btn_to_FormExercises
+            // 
+            this.btn_to_FormExercises.Location = new System.Drawing.Point(434, 230);
+            this.btn_to_FormExercises.Name = "btn_to_FormExercises";
+            this.btn_to_FormExercises.Size = new System.Drawing.Size(145, 23);
+            this.btn_to_FormExercises.TabIndex = 3;
+            this.btn_to_FormExercises.Text = "Gyakorlatok szerkesztése";
+            this.btn_to_FormExercises.UseVisualStyleBackColor = true;
+            this.btn_to_FormExercises.Click += new System.EventHandler(this.btn_to_FormExercises_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_to_FormExercises);
+            this.Controls.Add(this.btn_to_FormDoneWorkouts);
+            this.Controls.Add(this.btn_to_FormMeasures);
+            this.Controls.Add(this.btn_to_FormWorkoutSession);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_to_FormWorkoutSession;
+        private System.Windows.Forms.Button btn_to_FormMeasures;
+        private System.Windows.Forms.Button btn_to_FormDoneWorkouts;
+        private System.Windows.Forms.Button btn_to_FormExercises;
     }
 }
 
--- FittSoft/FittSoft/Form1.cs
+++ FittSoft/FittSoft/Form1.cs
@@ -16,5 +16,29 @@ namespace FittSoft
         {
             InitializeComponent();
         }
+
+        private void btn_to_FormWorkoutSession_Click(object sender, EventArgs e)
+        {
+            FormWorkoutSession form = new FormWorkoutSession();
+            form.ShowDialog();
+        }
+
+        private void btn_to_FormMeasures_Click(object sender, EventArgs e)
+        {
+            FormMeasures form = new FormMeasures();
+            form.ShowDialog();
+        }
+
+        private void btn_to_FormDoneWorkouts_Click(object sender, EventArgs e)
+        {
+            FormDoneWorkouts form = new FormDoneWorkouts();
+            form.ShowDialog();
+        }
+
+        private void btn_to_FormExercises_Click(object sender, EventArgs e)
+        {
+            FormExercises form = new FormExercises();
+            form.ShowDialog();
+        }
     }
 }
